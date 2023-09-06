10b1dd9f6888d550313bd23cb58f8a097d960afd
javiercoronatecmx <112840715+javiercoronatecmx@users.noreply.github.com>
2023-09-05 17:26:07 -0700

    Bases angular con modulos, inputs, outputs, servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
+import { HeroModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HeroModule,DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
